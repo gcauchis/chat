8fe5b423fe476b0d75706b77739ea1018fcdec47
gcauchis <gabriel.cauchis@gmail.com>
2012-03-05 20:23:58 +0100

    test login/register ok

--- server/src/test/java/com/gc/irc/server/test/ServerTest.java
+++ server/src/test/java/com/gc/irc/server/test/ServerTest.java
@@ -1,5 +1,8 @@
 package com.gc.irc.server.test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 
 import org.junit.After;
@@ -15,17 +18,33 @@ import com.gc.irc.common.connector.ConnectionThread;
 import com.gc.irc.common.protocol.IRCMessage;
 import com.gc.irc.common.protocol.chat.IRCMessageChat;
 import com.gc.irc.common.protocol.command.IRCMessageCommandLogin;
+import com.gc.irc.common.protocol.command.IRCMessageCommandRegister;
 import com.gc.irc.server.ServerStarter;
 import com.gc.irc.server.test.handler.LoginMessageHandler;
 
+/**
+ * The Class ServerTest.
+ */
 public class ServerTest {
 
+	/** The Constant SERVER_PORT. */
+	private static final int SERVER_PORT = 1973;
+
+	/** The starter thread. */
 	private static Thread starterThread;
 
+	/** The starter. */
 	private static ServerStarter starter;
 
+	/** The connection thread. */
 	private ConnectionThread connectionThread;
 
+	/**
+	 * Inits the.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
 	@BeforeClass
 	public static void init() throws InterruptedException {
 		starter = new ServerStarter();
@@ -38,9 +57,15 @@ public class ServerTest {
 		System.out.println("Server up");
 	}
 
+	/**
+	 * Prepare.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
 	@Before
 	public void prepare() throws InterruptedException {
-		connectionThread = new ConnectionThread(null, 1973);
+		connectionThread = new ConnectionThread(null, SERVER_PORT);
 		connectionThread.start();
 		System.out.println("Wait for connectionThread to be up");
 		while (!connectionThread.isInitialized()) {
@@ -49,16 +74,28 @@ public class ServerTest {
 		System.out.println("ConnectionThread up");
 	}
 
+	/**
+	 * Clean all.
+	 */
 	@AfterClass
 	public static void cleanAll() {
 		starterThread.interrupt();
 	}
 
+	/**
+	 * Clean.
+	 */
 	@After
 	public void clean() {
 		connectionThread.interrupt();
 	}
 
+	/**
+	 * Basic test.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
 	@Test
 	public void basicTest() throws InterruptedException {
 		for (int i = 0; i < 5; i++) {
@@ -68,19 +105,53 @@ public class ServerTest {
 		}
 	}
 
+	/**
+	 * Login.
+	 * 
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 */
 	@Test
 	public void login() throws InterruptedException {
-		connectionThread.setMessageHandler(new LoginMessageHandler());
+		final LoginMessageHandler messageHandler = new LoginMessageHandler();
+		connectionThread.setMessageHandler(messageHandler);
+		messageHandler.reset();
 		final IRCMessage login = new IRCMessageCommandLogin("test", "test");
 		sendMessage(connectionThread, login);
-		Thread.sleep(1000);
+		while (!messageHandler.isMessageRecieved()) {
+			Thread.sleep(300);
+		}
+		if (!messageHandler.isLoginValidated()) {
+			messageHandler.reset();
+			final IRCMessage register = new IRCMessageCommandRegister("test",
+					"test");
+			sendMessage(connectionThread, register);
+			while (!messageHandler.isMessageRecieved()) {
+				Thread.sleep(300);
+			}
+		}
+		assertTrue(messageHandler.isLoginValidated());
+		assertNotNull(messageHandler.getLogin());
 	}
 
+	/**
+	 * Send message.
+	 * 
+	 * @param messageSender
+	 *            the message sender
+	 * @param message
+	 *            the message
+	 */
 	private void sendMessage(final IIRCMessageSender messageSender,
 			final IRCMessage message) {
 		messageSender.send(message);
 	}
 
+	/**
+	 * Gets the basic message.
+	 * 
+	 * @return the basic message
+	 */
 	private IRCMessage getBasicMessage() {
 		return new IRCMessageChat(0,
 				Arrays.asList((IClientMessageLine) new BasicClientMessage(
--- server/src/test/java/com/gc/irc/server/test/handler/LoginMessageHandler.java
+++ server/src/test/java/com/gc/irc/server/test/handler/LoginMessageHandler.java
@@ -1,15 +1,29 @@
 package com.gc.irc.server.test.handler;
 
+import org.apache.log4j.Logger;
+
 import com.gc.irc.common.api.IIRCMessageHandler;
+import com.gc.irc.common.entity.IRCUser;
 import com.gc.irc.common.protocol.IRCMessage;
+import com.gc.irc.common.protocol.notice.IRCMessageNoticeLogin;
 
 /**
  * The Class LoginMessageHandler.
  */
 public class LoginMessageHandler implements IIRCMessageHandler {
 
+	/** The login. */
+	private IRCUser login;
+
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = Logger
+			.getLogger(LoginMessageHandler.class);
+
 	/** The login validated. */
 	boolean loginValidated = false;
+
+	/** The message recieved. */
+	boolean messageRecieved = false;
 
 	/*
 	 * (non-Javadoc)
@@ -19,7 +33,15 @@ public class LoginMessageHandler implements IIRCMessageHandler {
 	 * .IRCMessage)
 	 */
 	public void handle(final IRCMessage message) {
-
+		LOGGER.info("Message recived: " + message);
+		if (message instanceof IRCMessageNoticeLogin) {
+			login = ((IRCMessageNoticeLogin) message).getUser();
+			if (login != null) {
+				loginValidated = true;
+				LOGGER.info("USER : " + login.toStringXML(""));
+			}
+		}
+		messageRecieved = true;
 	}
 
 	/**
@@ -29,6 +51,33 @@ public class LoginMessageHandler implements IIRCMessageHandler {
 	 */
 	public boolean isLoginValidated() {
 		return loginValidated;
+	}
+
+	/**
+	 * Checks if is message recieved.
+	 * 
+	 * @return true, if is message recieved
+	 */
+	public boolean isMessageRecieved() {
+		return messageRecieved;
+	}
+
+	/**
+	 * Reset.
+	 */
+	public void reset() {
+		LOGGER.info("Reset");
+		messageRecieved = false;
+		loginValidated = false;
+	}
+
+	/**
+	 * Gets the login.
+	 * 
+	 * @return the login
+	 */
+	public IRCUser getLogin() {
+		return login;
 	}
 
 }
