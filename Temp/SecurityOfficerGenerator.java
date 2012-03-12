875f0b534ce531f229ce43e4e10d273cad22144d
Gabriel Cauchis <gabriel.cauchis@gmail.com>
2012-03-12 14:37:48 +0100

    upd main temp expl

--- Temp/SecurityOfficerGenerator.java
+++ Temp/SecurityOfficerGenerator.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
-import com.acp.vision.security.Utils.PropertiesUtils;
+import com.acp.vision.security.utils.PropertiesUtils;
 
 /**
  * The Class SecurityOfficerGenerator.
  */
 public class SecurityOfficerGenerator {
+
+    /** The Constant SPRING_CONFIG_FILE. */
+    private static final String SPRING_CONFIG_FILE = "officerGenerator-spring-config.xml";
+
+    /** The Constant OFFICER_GENERATOR_BEAN_ID. */
+    private static final String OFFICER_GENERATOR_BEAN_ID = "officerGenerator";
 
     /**
      * The main method.
@@ -17,13 +23,12 @@ public class SecurityOfficerGenerator {
      * @param args the arguments
      */
     public static void main(String[] args) {
-        final XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("officerGenerator-spring-config.xml"));
-        PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
+        final XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(SPRING_CONFIG_FILE));
+        final PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
         cfg.setProperties(PropertiesUtils.getProperties());
         cfg.postProcessBeanFactory(beanFactory);
-        final SecurityOfficerGeneratorRunnable officerGenerator = (SecurityOfficerGeneratorRunnable) beanFactory.getBean("officerGenerator");
+        final SecurityOfficerGeneratorRunnable officerGenerator = (SecurityOfficerGeneratorRunnable) beanFactory.getBean(OFFICER_GENERATOR_BEAN_ID);
         officerGenerator.run();
     }
-
 
 }
