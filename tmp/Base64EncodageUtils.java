55280d4a9ade544434749bcf2b55b79b751e9b10
Gabriel Cauchis <gabriel.cauchis@gmail.com>
2012-12-14 17:37:39 +0100

    tmp utils

---
+++ tmp/Base64EncodageUtils.java
@@ -0,0 +1,174 @@
+package com.acp.acs.common.security.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Class Base64EncodageUtils.
+ */
+public final class Base64EncodageUtils {
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Base64EncodageUtils.class);
+
+    /**
+     * Instantiates a new base64 encodage utils.
+     */
+    private Base64EncodageUtils() {
+        super();
+    }
+
+    /**
+     * Checks if is base64.
+     * 
+     * @param value
+     *            the value
+     * @return true, if is base64
+     */
+    public static boolean isBase64(final String value) {
+        return Base64.isBase64(value);
+    }
+
+    /**
+     * Compress and encode base64.
+     * 
+     * @param value
+     *            the value
+     * @return the string
+     */
+    public static String compressAndEncodeBase64(final String value) {
+        try {
+            return encodeBase64(compress(value));
+        } catch (final UnsupportedEncodingException e) {
+            LOGGER.warn("Fail to compress and encode value: {}", e.getMessage());
+            return value;
+        }
+    }
+
+    /**
+     * Compress.
+     * 
+     * @param value
+     *            the value
+     * @return the byte[]
+     * @throws UnsupportedEncodingException
+     *             the unsupported encoding exception
+     */
+    public static byte[] compress(final String value) throws UnsupportedEncodingException {
+        final byte[] input = value.getBytes("UTF-8");
+        final byte[] output = new byte[value.length()];
+        final Deflater compresser = new Deflater();
+        compresser.setInput(input);
+        compresser.finish();
+        final int compressedDataLength = compresser.deflate(output);
+        return Arrays.copyOf(output, compressedDataLength);
+    }
+
+    /**
+     * Encode base64.
+     * 
+     * @param value
+     *            the value
+     * @return the string
+     * @throws UnsupportedEncodingException
+     *             the unsupported encoding exception
+     */
+    public static String encodeBase64(final byte[] value) throws UnsupportedEncodingException {
+        return new String(Base64.encodeBase64(value), "UTF-8");
+    }
+
+    /**
+     * Encode base64.
+     * 
+     * @param value
+     *            the value
+     * @return the string
+     * @throws UnsupportedEncodingException
+     *             the unsupported encoding exception
+     */
+    public static String encodeBase64(final String value) throws UnsupportedEncodingException {
+        return encodeBase64(value.getBytes("UTF-8"));
+    }
+
+    /**
+     * Decode base64 and decompress.
+     * 
+     * @param value
+     *            the value
+     * @return the string
+     */
+    public static String decodeBase64AndDecompress(final String value) {
+        try {
+            return decompress(decodeBase64(value));
+        } catch (final UnsupportedEncodingException e) {
+            LOGGER.warn("Fail to decodeBase64AndDecompress value: {}", e.getMessage());
+            return value;
+        }
+    }
+
+    /**
+     * Decode base64.
+     * 
+     * @param value
+     *            the value
+     * @return the byte[]
+     * @throws UnsupportedEncodingException
+     *             the unsupported encoding exception
+     */
+    public static byte[] decodeBase64(final String value) throws UnsupportedEncodingException {
+        if (Base64.isBase64(value)) {
+            return decodeBase64(value.getBytes("UTF-8"));
+        }
+        return value.getBytes("UTF-8");
+    }
+
+    /**
+     * Decode base64.
+     * 
+     * @param value
+     *            the value
+     * @return the byte[]
+     * @throws UnsupportedEncodingException
+     *             the unsupported encoding exception
+     */
+    public static byte[] decodeBase64(final byte[] value) throws UnsupportedEncodingException {
+        return Base64.decodeBase64(value);
+    }
+
+    /**
+     * Decompress.
+     * 
+     * @param value
+     *            the value
+     * @return the string
+     */
+    public static String decompress(final byte[] value) {
+        String resultValue = null;
+        try {
+            resultValue = new String(value, "UTF-8");
+        } catch (final UnsupportedEncodingException e) {
+            LOGGER.warn("fail to build UTF-8 string", e);
+        }
+        try {
+            final Inflater decompresser = new Inflater();
+            decompresser.setInput(value, 0, value.length);
+            final byte[] result = new byte[value.length * 4];
+            final int resultLength = decompresser.inflate(result);
+            decompresser.end();
+
+            resultValue = new String(result, 0, resultLength, "UTF-8");
+        } catch (UnsupportedEncodingException | DataFormatException e) {
+            LOGGER.warn("fail to decodeBase64AndDecompress: {}", e.getMessage());
+        }
+        return resultValue;
+
+    }
+
+}
---
+++ tmp/HexaEncodageUtils.java
@@ -0,0 +1,78 @@
+package com.acp.acs.common.security.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * The Class HexaEncodageUtils.
+ */
+public final class HexaEncodageUtils {
+
+    /**
+     * Instantiates a new hexa encodage utils.
+     */
+    private HexaEncodageUtils() {
+        super();
+    }
+
+    /**
+     * Convert an Hexadecimal encoded String into a decoded byte array.
+     * 
+     * @param s
+     *            an Hexadecimal encoded String
+     * @return a decoded byte array
+     */
+    public static byte[] hexStringToByteArray(String s) {
+        if (StringUtils.isEmpty(s)) {
+            return new byte[0];
+        }
+        final int len = s.length();
+        final byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    /**
+     * Hex string to string.
+     * 
+     * @param input
+     *            the input
+     * @return the string
+     */
+    public static String hexStringToString(final String input) {
+        return new String(hexStringToByteArray(input));
+    }
+
+    /**
+     * Convert a byte array into an hexadecimal encoded string.
+     * 
+     * @param b
+     *            a byte array
+     * @return hexadecimal encoded string
+     * @throws Exception
+     *             the exception
+     */
+    public static String getHexString(final byte[] b) {
+        if (b == null || b.length <= 0) {
+            return "";
+        }
+        final StringBuffer result = new StringBuffer("");
+        for (int i = 0; i < b.length; i++) {
+            result.append(Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return result.toString();
+    }
+
+    /**
+     * Gets the hex string.
+     * 
+     * @param input
+     *            the input
+     * @return the hex string
+     */
+    public static String getHexString(final String input) {
+        return getHexString(input.getBytes());
+    }
+
+}
