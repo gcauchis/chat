b996e869d7dc09debd39919522bcf3e73c1ab7c0
gcauchis <gabriel.cauchis@gmail.com>
2014-12-22 16:20:32 +0100

    websocket next

--- server/server-connector/server-connector-websocket/src/main/java/com/gc/irc/server/client/connector/websocket/WebSocketClientConnector.java
+++ server/server-connector/server-connector-websocket/src/main/java/com/gc/irc/server/client/connector/websocket/WebSocketClientConnector.java
@@ -1,6 +1,10 @@
 package com.gc.irc.server.client.connector.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -16,11 +20,11 @@ import com.gc.irc.server.client.connector.ClientConnector;
  */
 @Configuration
 @EnableWebSocket
-public class WebSocketClientConnector extends AbstractClientConnector implements ClientConnector, WebSocketConfigurer {
+public class WebSocketClientConnector extends AbstractClientConnector implements ClientConnector, WebSocketConfigurer, WebSocketHandler {
 
 	@Override
 	public void initConnector() {
-		
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -31,8 +35,39 @@ public class WebSocketClientConnector extends AbstractClientConnector implements
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(this, "/");
+		
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session)
+			throws Exception {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean supportsPartialMessages() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 
