72655688f5dd414f74231ae27e9e60300f34f9dd
gcauchis <gabriel.cauchis@gmail.com>
2014-12-22 16:02:04 +0100

    add link

--- server/server-connector/server-connector-websocket/src/main/java/com/gc/irc/server/client/connector/websocket/WebSocketClientConnector.java
+++ server/server-connector/server-connector-websocket/src/main/java/com/gc/irc/server/client/connector/websocket/WebSocketClientConnector.java
@@ -1,18 +1,36 @@
 package com.gc.irc.server.client.connector.websocket;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 import com.gc.irc.server.client.connector.AbstractClientConnector;
 import com.gc.irc.server.client.connector.ClientConnector;
 
-public class WebSocketClientConnector extends AbstractClientConnector implements ClientConnector {
+/**
+ * 
+ * @author gcauchis
+ * @see http://docs.spring.io/spring/docs/current/spring-framework-reference/html/websocket.html
+ *
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketClientConnector extends AbstractClientConnector implements ClientConnector, WebSocketConfigurer {
 
 	@Override
 	public void initConnector() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void waitClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		
 	}
